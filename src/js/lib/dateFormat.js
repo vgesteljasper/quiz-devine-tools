7b6cdc50cc963942580abbd880795cfaa98f6a89
vgesteljasper <vgesteljasper@gmail.com>
2017-05-13 15:24:13 +0200

    ready for heroku

--- src/js/lib/dateFormat.js
+++
@@ -1,9 +0,0 @@
-import dateFormat from 'dateformat';
-
-export const toDate = date => {
-  return dateFormat(date, `longDate`);
-};
-
-export const toYear = date => {
-  return dateFormat(date, `yyyy`);
-};
